03b608637460bbeead992d929b1d121547baf8ba
Dylan Rosenthal <djrosent@ucsc.edu>
2022-10-26 20:43:26 -0700

    login wip

---
+++ auth/jwt.go
@@ -0,0 +1,52 @@
+package auth
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/google/uuid"
+)
+
+type locallyImaginedClaims struct {
+	Name  string
+	Email string
+	jwt.RegisteredClaims
+}
+
+func MakeToken(email string) (string, error) {
+	uuid := uuid.New()
+	claims := jwt.RegisteredClaims{
+		IssuedAt: jwt.NewNumericDate(time.Now()),
+		ID:       uuid.String()}
+
+	payload := &locallyImaginedClaims{
+		Name:             email,
+		Email:            email,
+		RegisteredClaims: claims,
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, payload)
+	signedJWT, err := token.SignedString([]byte("test"))
+	if err != nil {
+		return "", fmt.Errorf("unable to sign token with zendesk secret: %v", err)
+	}
+	return signedJWT, nil
+}
+
+func DecodeToken(tokenString string) *jwt.Token {
+	claims := jwt.MapClaims{}
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte("test"), nil
+	})
+	// ... error handling
+	if err != nil {
+		log.Println("NOOIOIO")
+	}
+	// do something with decoded claims
+	log.Println(token)
+	for key, val := range claims {
+		fmt.Printf("Key: %v, value: %v\n", key, val)
+	}
+	return token
+}
--- auth/login.go
+++ auth/login.go
@@ -1,34 +1,67 @@
-package login
+package auth
 
 import (
 	"backend/gen/auth"
 	"context"
+	"database/sql"
 	"fmt"
 	"log"
+	"net"
 	"os"
 
-	"github.com/GoogleCloudPlatform/cloudsql-proxy/proxy/dialers/mysql"
+	"cloud.google.com/go/cloudsqlconn"
+	"github.com/go-sql-driver/mysql"
 )
 
 type Service struct{}
 
 func (s *Service) Login(ctx context.Context, p *auth.LoginPayload) (string, error) {
-	cfg := mysql.Cfg(os.Getenv("INSTANCE")+"-m", os.Getenv("DBUSER"), os.Getenv("DBPASS"))
-	cfg.DBName = os.Getenv("DBNAME")
-	db, err := mysql.DialCfg(cfg)
+	// Note: Saving credentials in environment variables is convenient, but not
+	// secure - consider a more secure solution such as
+	// Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
+	// keep secrets safe.
+	var (
+		dbUser                 = os.Getenv("DBUSER")   // e.g. 'my-db-user'
+		dbPwd                  = os.Getenv("DBPASS")   // e.g. 'my-db-password'
+		dbName                 = os.Getenv("DBNAME")   // e.g. 'my-database'
+		instanceConnectionName = os.Getenv("INSTANCE") // e.g. 'project:region:instance'
+	)
+
+	d, err := cloudsqlconn.NewDialer(context.Background())
 	if err != nil {
-		log.Fatal(err)
+		return "", fmt.Errorf("cloudsqlconn.NewDialer: %v", err)
 	}
-	defer db.Close()
-	err = db.Ping()
+	mysql.RegisterDialContext("cloudsqlconn",
+		func(ctx context.Context, addr string) (net.Conn, error) {
+			return d.Dial(ctx, instanceConnectionName)
+		})
+
+	dbURI := fmt.Sprintf("%s:%s@cloudsqlconn(localhost:3306)/%s?parseTime=true",
+		dbUser, dbPwd, dbName)
+
+	dbPool, err := sql.Open("mysql", dbURI)
 	if err != nil {
-		log.Fatal(err)
+		return "", fmt.Errorf("sql.Open: %v", err)
+	}
+	var password string
+	// Query for a value based on a single row.
+	row, err := dbPool.Query("SELECT password from test_table where username='" + *p.Username + "'")
+	if err == sql.ErrNoRows {
+		return "", fmt.Errorf("line 49")
+	}
+	// return "", fmt.Errorf("%w", row)
+	for row.Next() {
+		if err := row.Scan(&password); err != nil {
+			log.Fatal(err)
+		}
 	}
 
-	_, err = db.Exec(fmt.Sprintf("USE %s", cfg.DBName))
-	if err != nil {
-		log.Fatal(err)
+	if *p.Password != password {
+		return "BADPASSWORD", nil
 	}
-	return "hi", err
-
+	token, err := MakeToken(*p.Username)
+	if err != nil {
+		return "", err
+	}
+	return token, nil
 }
