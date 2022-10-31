282b978af9bb4e99385d5f0fe95b10bc3749e925
Dylan Rosenthal <djrosent@ucsc.edu>
2022-10-31 16:32:55 -0700

    signup too

--- auth/login.go
+++ auth/login.go
@@ -8,11 +8,8 @@ import (
 	"encoding/hex"
 	"fmt"
 	"log"
-	"net"
 	"os"
 
-	"cloud.google.com/go/cloudsqlconn"
-	"github.com/go-sql-driver/mysql"
 	_ "github.com/lib/pq"
 )
 
@@ -49,30 +46,7 @@ func (s *Service) Login(ctx context.Context, p *auth.LoginPayload) (string, erro
 }
 
 func (s *Service) Signup(ctx context.Context, p *auth.SignupPayload) (string, error) {
-	// Note: Saving credentials in environment variables is convenient, but not
-	// secure - consider a more secure solution such as
-	// Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
-	// keep secrets safe.
-	var (
-		dbUser                 = os.Getenv("DBUSER")   // e.g. 'my-db-user'
-		dbPwd                  = os.Getenv("DBPASS")   // e.g. 'my-db-password'
-		dbName                 = os.Getenv("DBNAME")   // e.g. 'my-database'
-		instanceConnectionName = os.Getenv("INSTANCE") // e.g. 'project:region:instance'
-	)
-
-	d, err := cloudsqlconn.NewDialer(context.Background())
-	if err != nil {
-		return "", fmt.Errorf("cloudsqlconn.NewDialer: %v", err)
-	}
-	mysql.RegisterDialContext("cloudsqlconn",
-		func(ctx context.Context, addr string) (net.Conn, error) {
-			return d.Dial(ctx, instanceConnectionName)
-		})
-
-	dbURI := fmt.Sprintf("%s:%s@cloudsqlconn(localhost:3306)/%s?parseTime=true",
-		dbUser, dbPwd, dbName)
-
-	dbPool, err := sql.Open("mysql", dbURI)
+	dbPool, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
 	if err != nil {
 		return "", fmt.Errorf("sql.Open: %v", err)
 	}
@@ -80,7 +54,7 @@ func (s *Service) Signup(ctx context.Context, p *auth.SignupPayload) (string, er
 	hashedPassword := shaHashing(*p.Password)
 	var value string = ""
 	// Query for a value based on a single row.
-	row, err := dbPool.Query("SELECT username from test_table where username='" + *p.Username + "'")
+	row, err := dbPool.Query("SELECT username from test_table where username=$1", *p.Username)
 	if err != nil {
 		return "", err
 	}
