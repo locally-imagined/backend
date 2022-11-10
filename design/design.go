734c80faa5e14435424b5d3ded098d6f51bcbd3a
Dylan Rosenthal <djrosent@ucsc.edu>
2022-11-09 17:56:43 -0800

    postings might work

--- design/design.go
+++ design/design.go
@@ -36,7 +36,7 @@ var Post = Type("Post", func() {
 	Attribute("title", String, "Post title")
 	Attribute("description", String, "Post description")
 	Attribute("price", String, "Post price")
-	Attribute("content", Bytes, "Post content")
+	Attribute("content", String, "Post content")
 	Required("title", "description", "price", "content")
 })
 
