10c989987856bc034d81aff521a1d48cf7913086
Dylan Rosenthal <djrosent@ucsc.edu>
2022-11-21 04:46:45 -0800

    search bar getpage

--- postings/postings.go
+++ postings/postings.go
@@ -40,7 +40,7 @@ var (
 	GETPOSTPAGEWITHKEYWORD string = `SELECT p.postid, p.userid, p.title, p.description, 
 					p.price, p.medium, p.sold, p.uploaddate, i.imgid FROM posts AS p LEFT 
 					JOIN images AS i ON p.postid = i.postid WHERE i.index=0 AND 
-					((LOWER(p.title) LIKE '%$1::varchar%') OR (LOWER(p.description) LIKE '%$2%')) 
+					((LOWER(p.title) LIKE CONCAT('%', $1) OR (LOWER(p.description) LIKE '%$2%')) 
 					ORDER BY p.uploaddate OFFSET $3 ROWS FETCH NEXT 25 ROWS ONLY`
 	SELECTIMAGES    string = "SELECT imgid from images where postid=$1 ORDER BY index"
 	SELECTUSERID    string = "SELECT userID from Posts where postID=$1"
@@ -186,7 +186,7 @@ func (s *Service) GetPostPage(ctx context.Context, p *postings.GetPostPagePayloa
 			return nil, err
 		}
 		defer stmt.Close()
-		rows, err = stmt.Query(string(*p.Keyword), *p.Keyword, offset)
+		rows, err = stmt.Query((*p.Keyword), *p.Keyword, offset)
 	} else {
 		stmt, err = dbPool.Prepare(GETPOSTPAGE)
 		if err != nil {
