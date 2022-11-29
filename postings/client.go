7abfe010feaf159f9c727cf98b36e5b0f7cd49a7
Dylan Rosenthal <djrosent@ucsc.edu>
2022-11-28 18:05:15 -0800

    restructure for mocks

--- postings/client.go
+++ postings/client.go
@@ -1,85 +1,187 @@
 package postings
 
-// TODO
-// add delivery type column to db and add support for it in createpost and editpost endpoints
-// add mark as sold option to ui and change editpost endpoint to support it
-// add artist bio column to users default empty and create endpoint to set user bio
-// decide on filters: art type, price, sold?
-
-// should be package postings
 import (
-	"backend/auth"
 	"backend/gen/postings"
 	"context"
 	"database/sql"
 	"log"
+	"os"
 	"strings"
 	"time"
 
-	"github.com/golang-jwt/jwt/v4"
-
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/google/uuid"
 	_ "github.com/lib/pq"
-	"goa.design/goa/v3/security"
 )
 
-type Service struct {
-	client client
+type (
+	// client is the zendesk service client interface
+	Client interface {
+		CreatePost(ctx context.Context, p *postings.CreatePostPayload) (*postings.CreatePostResult, error)
+
+		GetPostPage(ctx context.Context, p *postings.GetPostPagePayload) (*postings.GetPostPageResult, error)
+
+		GetArtistPostPage(ctx context.Context, p *postings.GetArtistPostPagePayload) (*postings.GetArtistPostPageResult, error)
+
+		GetPostPageFiltered(ctx context.Context, p *postings.GetPostPageFilteredPayload) (*postings.GetPostPageFilteredResult, error)
+
+		GetImagesForPost(ctx context.Context, p *postings.GetImagesForPostPayload) (*postings.GetImagesForPostResult, error)
+
+		DeletePost(ctx context.Context, p *postings.DeletePostPayload) error
+
+		EditPost(ctx context.Context, p *postings.EditPostPayload) (*postings.EditPostResult, error)
+	}
+
+	client struct{}
+)
+
+func New() Client {
+	return &client{}
 }
 
-func (s *Service) JWTAuth(ctx context.Context, token string, scheme *security.JWTScheme) (context.Context, error) {
-	tok := auth.DecodeToken(token)
-	if tok == nil {
-		return ctx, ErrUnauthorized
-	}
-	// 3. add authInfo to context
-	claims := tok.Claims.(jwt.MapClaims)
-	ctx = context.WithValue(ctx, "Username", claims["Username"])
-	ctx = context.WithValue(ctx, "UserID", claims["UserID"])
-	var exp time.Time
-	var now time.Time = time.Now()
-	switch iat := claims["iat"].(type) {
-	case float64:
-		exp = time.Unix(int64(iat), 0)
-	}
-	if exp.Add(time.Hour * 2).Before(now) {
-		return ctx, ErrUnauthorized
-	}
+var (
+	// ErrUnauthorized is the error returned by Login when the request credentials
+	// are invalid.
 
-	return ctx, nil
+	// add prepares before queries
+	//
+	INSERTPOST   string = "INSERT INTO Posts (postid, userid, title, description, price, medium, deliverytype) Values ($1, $2, $3, $4, $5, $6, $7)"
+	INSERTIMAGES string = "INSERT INTO Images Values ($1, $2, $3)"
+	GETUSERNAME  string = "SELECT username FROM users WHERE userid=$1"
+	GETPOSTPAGE  string = `SELECT p.postid, p.userid, u.username, p.title, p.description, 
+					p.price, p.medium, p.sold, p.uploaddate, p.deliverytype, i.imgid FROM posts AS p
+					LEFT JOIN (SELECT imgid, postid FROM images WHERE index=0) AS i ON p.postid=i.postid 
+					LEFT JOIN users AS u ON p.userid = u.userid
+					ORDER BY p.uploaddate OFFSET $1 ROWS FETCH NEXT 25 ROWS ONLY`
+	GETPOSTPAGEWITHKEYWORD string = `SELECT p.postid, p.userid, p.title, p.description, 
+					p.price, p.medium, p.sold, p.uploaddate, p.deliverytype, i.imgid FROM posts AS p LEFT 
+					JOIN images AS i ON p.postid = i.postid WHERE i.index=0 AND 
+					((LOWER(p.title) LIKE $1) OR (LOWER(p.description) LIKE $2))
+					ORDER BY p.uploaddate OFFSET $3 ROWS FETCH NEXT 25 ROWS ONLY`
+	GETPOSTPAGEFORARTIST string = `SELECT p.postid, p.userid, u.username, p.title, 
+					p.description, p.price, p.medium, p.sold, p.uploaddate, p.deliverytype, i.imgid FROM posts AS p
+					LEFT JOIN (SELECT imgid, postid FROM images WHERE index=0) AS i ON p.postid=i.postid 
+					LEFT JOIN users AS u ON p.userid = u.userid where p.userid=$1      
+					ORDER BY p.uploaddate OFFSET $2 ROWS FETCH NEXT 25 ROWS ONLY;`
+	SELECTIMAGES    string = "SELECT imgid from images where postid=$1 ORDER BY index"
+	SELECTUSERID    string = "SELECT userID from Posts where postID=$1"
+	DELETEIMAGES    string = "DELETE FROM images WHERE postID=$1"
+	DELETEPOST      string = "DELETE FROM posts WHERE postID=$1"
+	DELETEIMAGE     string = "DELETE FROM images where imgid=$1"
+	UPDATEINDEX     string = "UPDATE images SET index = index - 1 WHERE (postid=$1 AND index>(SELECT index FROM images WHERE imgid=$2))"
+	ADDIMAGE        string = "INSERT INTO images VALUES($1, $2, (SELECT MAX(index) FROM images where postID=$3) + 1)"
+	GETEDITEDINFO   string = "SELECT title, description, price, medium, sold, deliverytype, uploaddate FROM posts where postID=$1"
+	IMAGESPERPAGE   int    = 25
+	ErrUnauthorized error  = postings.Unauthorized("invalid jwt")
+)
+
+func getS3Session() (string, *s3.S3) {
+	awsAccessKey := os.Getenv("BUCKETEER_AWS_ACCESS_KEY_ID")
+	awsSecretKey := os.Getenv("BUCKETEER_AWS_SECRET_ACCESS_KEY")
+	awsRegion := os.Getenv("BUCKETEER_AWS_REGION")
+	awsBucketName := os.Getenv("BUCKETEER_BUCKET_NAME")
+	sess := session.Must(session.NewSession(&aws.Config{
+		Region:      aws.String(awsRegion),
+		Credentials: credentials.NewStaticCredentials(awsAccessKey, awsSecretKey, ""),
+	}))
+	// create a new instance of the service's client with a session.
+	svc := s3.New(sess)
+	return awsBucketName, svc
+}
+
+func putImageToS3(ctx context.Context, svc *s3.S3, awsBucketName, imageID string, reader *strings.Reader) error {
+	_, err := svc.PutObjectWithContext(ctx, &s3.PutObjectInput{
+		Bucket: aws.String(awsBucketName),
+		Key:    aws.String("public/" + imageID),
+		Body:   reader,
+	})
+	return err
+}
+
+func deleteImageFromS3(ctx context.Context, svc *s3.S3, awsBucketName, imageID string) error {
+	_, err := svc.DeleteObjectWithContext(ctx, &s3.DeleteObjectInput{
+		Bucket: aws.String(awsBucketName),
+		Key:    aws.String("public/" + imageID),
+	})
+	return err
+}
+
+func openDB() (*sql.DB, error) {
+	return sql.Open("postgres", os.Getenv("DATABASE_URL"))
 }
 
 // maybe i can mock openDB so that i am working with a mock db
 // make opendb a service method and mock it?
 // or restructure and make service/client files and mock the client entirely and dont do client tests
 // under postings directory, just have service.go and client.go and test service methods with mocked clients
-func (s *Service) CreatePost(ctx context.Context, p *postings.CreatePostPayload) (*postings.CreatePostResult, error) {
-	res, err := s.client.CreatePost(ctx, p)
+func (c *client) CreatePost(ctx context.Context, p *postings.CreatePostPayload) (*postings.CreatePostResult, error) {
+	awsBucketName, svc := getS3Session()
+	postID := uuid.New().String()
+	dbPool, err := openDB()
 	if err != nil {
 		return nil, err
+	}
+	defer dbPool.Close()
+	now := time.Now().Format(time.RFC3339)
+	_, err = dbPool.Query(INSERTPOST, postID, ctx.Value("UserID").(string), p.Post.Title, p.Post.Description, p.Post.Price, p.Post.Medium, p.Post.Deliverytype)
+	if err != nil {
+		return nil, err
+	}
+	imageIDs := make([]string, 0)
+	for index, content := range p.Post.Content {
+		imageID := uuid.New().String()
+		imageIDs = append(imageIDs, imageID)
+		reader := strings.NewReader(string(content))
+		// put the object in the bucket
+		err := putImageToS3(ctx, svc, awsBucketName, imageID, reader)
+		if err != nil {
+			return nil, err
+		}
+		_, err = dbPool.Query(INSERTIMAGES, imageID, postID, index)
+		if err != nil {
+			return nil, err
+		}
+	}
+	posted := &postings.PostResponse{
+		Title:        p.Post.Title,
+		Description:  p.Post.Description,
+		Price:        p.Post.Price,
+		ImageIDs:     imageIDs,
+		PostID:       postID,
+		Medium:       p.Post.Medium,
+		Sold:         false,
+		UploadDate:   now[0:10],
+		Deliverytype: p.Post.Deliverytype,
+		UserID:       ctx.Value("UserID").(string),
+	}
+	res := &postings.CreatePostResult{
+		Posted: posted,
 	}
 	return res, nil
 }
 
-// type image struct {
-// 	imageID string
-// }
+type image struct {
+	imageID string
+}
 
-// type post struct {
-// 	postID       string
-// 	userID       string
-// 	postTitle    string
-// 	postDesc     string
-// 	price        string
-// 	uploadDate   string
-// 	imageID      string
-// 	medium       string
-// 	sold         bool
-// 	deliverytype string
-// 	username     string
-// }
+type post struct {
+	postID       string
+	userID       string
+	postTitle    string
+	postDesc     string
+	price        string
+	uploadDate   string
+	imageID      string
+	medium       string
+	sold         bool
+	deliverytype string
+	username     string
+}
 
-func (s *Service) GetPostPage(ctx context.Context, p *postings.GetPostPagePayload) (*postings.GetPostPageResult, error) {
+func (c *client) GetPostPage(ctx context.Context, p *postings.GetPostPagePayload) (*postings.GetPostPageResult, error) {
 	dbPool, err := openDB()
 	if err != nil {
 		return nil, err
@@ -105,7 +207,7 @@ func (s *Service) GetPostPage(ctx context.Context, p *postings.GetPostPagePayloa
 	return &postings.GetPostPageResult{Posts: res}, err
 }
 
-func (s *Service) GetArtistPostPage(ctx context.Context, p *postings.GetArtistPostPagePayload) (*postings.GetArtistPostPageResult, error) {
+func (c *client) GetArtistPostPage(ctx context.Context, p *postings.GetArtistPostPagePayload) (*postings.GetArtistPostPageResult, error) {
 	dbPool, err := openDB()
 	if err != nil {
 		return nil, err
@@ -131,7 +233,7 @@ func (s *Service) GetArtistPostPage(ctx context.Context, p *postings.GetArtistPo
 	return &postings.GetArtistPostPageResult{Posts: res}, err
 }
 
-func (s *Service) GetPostPageFiltered(ctx context.Context, p *postings.GetPostPageFilteredPayload) (*postings.GetPostPageFilteredResult, error) {
+func (c *client) GetPostPageFiltered(ctx context.Context, p *postings.GetPostPageFilteredPayload) (*postings.GetPostPageFilteredResult, error) {
 	dbPool, err := openDB()
 	if err != nil {
 		return nil, err
@@ -178,7 +280,7 @@ func (s *Service) GetPostPageFiltered(ctx context.Context, p *postings.GetPostPa
 	return &postings.GetPostPageFilteredResult{Posts: res}, err
 }
 
-func (s *Service) GetImagesForPost(ctx context.Context, p *postings.GetImagesForPostPayload) (*postings.GetImagesForPostResult, error) {
+func (c *client) GetImagesForPost(ctx context.Context, p *postings.GetImagesForPostPayload) (*postings.GetImagesForPostResult, error) {
 	dbPool, err := openDB()
 	if err != nil {
 		return nil, err
@@ -200,7 +302,7 @@ func (s *Service) GetImagesForPost(ctx context.Context, p *postings.GetImagesFor
 	return &postings.GetImagesForPostResult{Images: res}, err
 }
 
-func (s *Service) DeletePost(ctx context.Context, p *postings.DeletePostPayload) error {
+func (c *client) DeletePost(ctx context.Context, p *postings.DeletePostPayload) error {
 	dbPool, err := openDB()
 	if err != nil {
 		return err
@@ -254,7 +356,7 @@ func (s *Service) DeletePost(ctx context.Context, p *postings.DeletePostPayload)
 	return nil
 }
 
-func (s *Service) EditPost(ctx context.Context, p *postings.EditPostPayload) (*postings.EditPostResult, error) {
+func (c *client) EditPost(ctx context.Context, p *postings.EditPostPayload) (*postings.EditPostResult, error) {
 	dbPool, err := openDB()
 	if err != nil {
 		return nil, err
